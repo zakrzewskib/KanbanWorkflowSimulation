d4253eecb109f6a986f7bceebb5f3309e8be2552
Bartosz <barzak200@gmail.com>
2021-04-15 17:14:36 +0200

    Add 3 types of tasks

--- src/components/AddTask.vue
+++ src/components/AddTask.vue
@@ -8,11 +8,27 @@
 export default {
   name: "AddTask",
   methods: {
+    randomIntFromInterval(min, max) {
+      // min and max included
+      return Math.floor(Math.random() * (max - min + 1) + min);
+    },
+
     onClick() {
+      var tasksTypes = ["Normal task", "Urgent task", "Fixed date task"];
+      var random = this.randomIntFromInterval(1, 100);
+      var index;
+      if(random <= 50) {
+        index = 0;
+      } else if (random > 50 && random <= 75) {
+        index = 1;
+      } else {
+        index = 2;
+      }
+
       const newTask = {
         id: Math.random(),
-        name: "Normal task",
-        day: "11.11",
+        name: tasksTypes[index],
+        // day: "11.11",
       };
       // console.log(newTask);
       this.$emit("add-task", newTask);
